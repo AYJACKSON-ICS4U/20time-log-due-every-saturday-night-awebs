3d5c4668e24851c986db543f16b35dd0971cb129
awebs <awebs1@ocdsb.ca>
2017-10-26 17:34:01 -0400

    Adding Week 4 Project Files

---
+++ XLSgen/src/converter/Converter.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package converter;
+
+/**
+ *
+ * @author awebster
+ */
+public class Converter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       ViewController viewCon = new ViewController();
+       viewCon.setVisible(true);
+    }
+    
+}
